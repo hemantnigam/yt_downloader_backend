4d943f999296c4478ba4a18be530c826bbaa1bf3
Hemant Nigam <hemant.nigam@nagarro.com>
2020-07-10 21:38:48 +0530

    file_changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const itags = require("./constants/itags");
 const cors = require("cors");
 const ytdl = require("ytdl-core");
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
